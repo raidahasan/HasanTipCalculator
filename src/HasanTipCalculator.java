be42d9d0851fef5ff6a11ec051746e3081431f33
Raida Hasan <raidahasan09@gmail.com>
2023-09-21 19:58:00 -0400

    Second Commit

--- src/HasanTipCalculator.java
+++ src/HasanTipCalculator.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class HasanTipCalculator {
+import java.util.Scanner;
+
+public class HasanTipCalculator {
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to the Tip Calculator!");
+        System.out.println("Please enter the amount of people with you (including yourself) today: ");
+        int people = scan.nextInt();
+        scan.nextLine();
+        System.out.println("Enter the costs of each food on the next line, enter -1 when done");
+        double bill = scan.nextDouble();
+
+
+
+
+
+    }
 }
